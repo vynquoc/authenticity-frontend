be2737d34202ec0949bba1306c5296269e1c8bd0
vynquoc <vynquoc@gmail.com>
2023-07-30 19:33:59 +0700

    fix api url

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,28 +1,34 @@
-import axios from 'axios'
-import queryString from 'query-string'
+import axios from "axios";
+import queryString from "query-string";
 //default config for http requests
 
 const axiosClient = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
-    headers: {
-        'content-type': 'application/json',
-    },
-    paramsSerializer: params => queryString.stringify(params),
-})
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:8000/api"
+      : process.env.REACT_APP_API_URL,
+  headers: {
+    "content-type": "application/json",
+  },
+  paramsSerializer: (params) => queryString.stringify(params),
+});
 
 axiosClient.interceptors.request.use(async (config) => {
-    const token = localStorage.getItem('token')
-    config.headers.Authorization = `Bearer ${token}`
-    return config
-})
+  const token = localStorage.getItem("token");
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
-axiosClient.interceptors.response.use((response) => {
+axiosClient.interceptors.response.use(
+  (response) => {
     if (response && response.data) {
-        return response.data
+      return response.data;
     }
-    return response
-}, (error) => {
-    throw error
-})
+    return response;
+  },
+  (error) => {
+    throw error;
+  }
+);
 
-export default axiosClient
+export default axiosClient;
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,62 +1,60 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import productApi from '../api/productApi'
-import { useTranslation } from 'react-i18next'
-import Skeleton from 'react-loading-skeleton'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import productApi from "../api/productApi";
+import { useTranslation } from "react-i18next";
+import Skeleton from "react-loading-skeleton";
 //components
-import Product from '../features/Product/components/Product'
+import Product from "../features/Product/components/Product";
 
-const Category = ({title, link, recent, category}) => {
-    const {t} = useTranslation()
-    const [products, setProducts] = useState([])
-    const [loading, setLoading] = useState(true)
-    const getProducts = async () => {
-        try {
-            if (category === 'sneakers') {
-                const response = await productApi.getNewestSneakers()
-                setProducts(response.data)
-                setLoading(false)
-            } else if ( category === 'streetwear') {
-                const response = await productApi.getNewestStreetwear()
-                setProducts(response.data)
-                setLoading(false)
-
-            } else if ( category === 'collectibles') {
-                const response = await productApi.getNewestCollectibles()
-                setProducts(response.data)
-                setLoading(false)
-
-            } else {
-                const storageItem = sessionStorage.getItem('viewedProduct')
-                const viewedProduct = JSON.parse(storageItem)
-                let arr = []
-                for (const obj of viewedProduct) {
-                    arr.push(obj)
-                }
-                setLoading(false)
-
-                setProducts(arr)
-            }
-        } catch (error) {
-            setLoading(false)
-
-            console.log(error.response)
+const Category = ({ title, link, recent, category }) => {
+  const { t } = useTranslation();
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const getProducts = async () => {
+    try {
+      if (category === "sneakers") {
+        const response = await productApi.getNewestSneakers();
+        setProducts(response.data);
+        setLoading(false);
+      } else if (category === "streetwear") {
+        const response = await productApi.getNewestStreetwear();
+        setProducts(response.data);
+        setLoading(false);
+      } else if (category === "collectibles") {
+        const response = await productApi.getNewestCollectibles();
+        setProducts(response.data);
+        setLoading(false);
+      } else {
+        const storageItem = sessionStorage.getItem("viewedProduct");
+        const viewedProduct = JSON.parse(storageItem);
+        let arr = [];
+        for (const obj of viewedProduct) {
+          arr.push(obj);
         }
-    } 
-    useEffect(() => {
-        getProducts()
-       
-    }, [getProducts])
-    return (
-        <div className="w-full mt-5">
-            <div className="w-full flex justify-between items-center">
-                <span className="text-lg font-semibold">{title}</span>
-                {
-                    !recent && <Link to={link} className="text-sm font-medium">{t("seeAll")}</Link>
-                }
-            </div>
-            <div className="grid grid-cols-5 gap-4">
-                {/* {
+        setLoading(false);
+        setProducts(arr);
+      }
+    } catch (error) {
+      setLoading(false);
+
+      console.log(error.response);
+    }
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
+  return (
+    <div className="w-full mt-5">
+      <div className="w-full flex justify-between items-center">
+        <span className="text-lg font-semibold">{title}</span>
+        {!recent && (
+          <Link to={link} className="text-sm font-medium">
+            {t("seeAll")}
+          </Link>
+        )}
+      </div>
+      <div className="grid grid-cols-5 gap-4">
+        {/* {
                     products.map((product, index) => <Product 
                     key={index}
                     name={product?.name}
@@ -65,51 +63,63 @@ const Category = ({title, link, recent, category}) => {
                     slug={product?.slug} 
                     />)
                 } */}
-                {
-                    !loading ? products.map((product, index) => <Product 
-                    key={index}
-                    name={product?.name}
-                    price={product?.averagePrice}
-                    image={product?.images.smallImageUrl}
-                    slug={product?.slug} 
-                    />) : 
-                    <>
-                        <div>
-                            <Skeleton height={180} />
-                            <Skeleton />
-                            <p className="capitalize text-xs text-gray-500">Giá bán trung bình</p>
-                            <Skeleton width={100} />
-                        </div>
-                        <div>
-                            <Skeleton height={180} />
-                            <Skeleton />
-                            <p className="capitalize text-xs text-gray-500">Giá bán trung bình</p>
-                            <Skeleton width={100} />
-                        </div>
-                        <div>
-                            <Skeleton height={180} />
-                            <Skeleton />
-                            <p className="capitalize text-xs text-gray-500">Giá bán trung bình</p>
-                            <Skeleton width={100} />
-                        </div>
-                        <div>
-                            <Skeleton height={180} />
-                            <Skeleton />
-                            <p className="capitalize text-xs text-gray-500">Giá bán trung bình</p>
-                            <Skeleton width={100} />
-                        </div>
-                        <div>
-                            <Skeleton height={180} />
-                            <Skeleton />
-                            <p className="capitalize text-xs text-gray-500">Giá bán trung bình</p>
-                            <Skeleton width={100} />
-                        </div>
-                    </>
-                     
-                }
+        {!loading ? (
+          products.map((product, index) => (
+            <Product
+              key={index}
+              name={product?.name}
+              price={product?.averagePrice}
+              image={product?.images.smallImageUrl}
+              slug={product?.slug}
+            />
+          ))
+        ) : (
+          <>
+            <div>
+              <Skeleton height={180} />
+              <Skeleton />
+              <p className="capitalize text-xs text-gray-500">
+                Giá bán trung bình
+              </p>
+              <Skeleton width={100} />
             </div>
-        </div>
-    )
-}
+            <div>
+              <Skeleton height={180} />
+              <Skeleton />
+              <p className="capitalize text-xs text-gray-500">
+                Giá bán trung bình
+              </p>
+              <Skeleton width={100} />
+            </div>
+            <div>
+              <Skeleton height={180} />
+              <Skeleton />
+              <p className="capitalize text-xs text-gray-500">
+                Giá bán trung bình
+              </p>
+              <Skeleton width={100} />
+            </div>
+            <div>
+              <Skeleton height={180} />
+              <Skeleton />
+              <p className="capitalize text-xs text-gray-500">
+                Giá bán trung bình
+              </p>
+              <Skeleton width={100} />
+            </div>
+            <div>
+              <Skeleton height={180} />
+              <Skeleton />
+              <p className="capitalize text-xs text-gray-500">
+                Giá bán trung bình
+              </p>
+              <Skeleton width={100} />
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Category
+export default Category;
